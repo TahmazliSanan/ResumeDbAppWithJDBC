5e9718a4ffb950d88de1ea602cd4980763c4c3dc
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-10-09 17:19:57 +0400

    Modify constructor

--- src/main/java/com/mycompany/entity/User.java
+++ src/main/java/com/mycompany/entity/User.java
@@ -22,12 +22,13 @@ public class User {
         this.id = id;
     }
 
-    public User(int id, String name, String surname, String email, String phone, Date birthDate, Country birthPlace, Country nationality) {
+    public User(int id, String name, String surname, String email, String phone, String profileDescription, Date birthDate, Country birthPlace, Country nationality) {
         this.id = id;
         this.name = name;
         this.surname = surname;
         this.email = email;
         this.phone = phone;
+        this.profileDescription = profileDescription;
         this.birthDate = birthDate;
         this.birthPlace = birthPlace;
         this.nationality = nationality;
