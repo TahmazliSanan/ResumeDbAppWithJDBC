2703b03ddf6c727324cbfcdce4ce8a7dd96473d2
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-10-11 13:23:30 +0400

    Modify method

--- src/main/java/com/mycompany/entity/Country.java
+++ src/main/java/com/mycompany/entity/Country.java
@@ -40,6 +40,6 @@ public class Country {
 
     @Override
     public String toString() {
-        return "Country{" + "id=" + id + ", name=" + name + ", nationality=" + nationality + '}';
+        return name + " (" + nationality + ")";
     }
 }
