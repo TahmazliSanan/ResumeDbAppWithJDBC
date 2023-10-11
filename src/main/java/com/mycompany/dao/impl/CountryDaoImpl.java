de4c1017948e45e66c7bc109bfde4e7b7707df3a
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-10-11 14:40:16 +0400

    Complete methods

--- src/main/java/com/mycompany/dao/impl/CountryDaoImpl.java
+++ src/main/java/com/mycompany/dao/impl/CountryDaoImpl.java
@@ -4,6 +4,7 @@ import com.mycompany.dao.inter.AbstractDao;
 import com.mycompany.dao.inter.CountryDaoInter;
 import com.mycompany.entity.Country;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -21,6 +22,41 @@ public class CountryDaoImpl extends AbstractDao implements CountryDaoInter {
     }
     
     @Override
+    public boolean addCountry(Country country) {
+        try (Connection con = connect()) {
+            PreparedStatement stmt = con.prepareStatement("INSERT INTO"
+                    + " COUNTRIES(NAME, NATIONALITY)"
+                    + " VALUES(?, ?)");
+            stmt.setString(1, country.getName());
+            stmt.setString(2, country.getNationality());
+            return stmt.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(CountryDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    
+    @Override
+    public Country getCountryById(int id) {
+        Country result = null;
+        try (Connection con = connect()) {
+            Statement stmt = con.createStatement();        
+            stmt.execute("""
+                            SELECT *
+                            FROM COUNTRIES
+                            WHERE ID = 
+                        """ + id);
+            ResultSet rs = stmt.getResultSet();
+            while (rs.next()) {
+                result = getCountry(rs);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CountryDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+    
+    @Override
     public List<Country> getAllCountries() {
         List<Country> result = new ArrayList<>();
         try (Connection con = connect()) {
@@ -34,8 +70,41 @@ public class CountryDaoImpl extends AbstractDao implements CountryDaoInter {
                 result.add(getCountry(rs));
             }
         } catch (SQLException ex) {
-            Logger.getLogger(UserDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(CountryDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
         return result;
+    }
+
+    @Override
+    public boolean updateCountry(Country country) {
+        try (Connection con = connect()) {
+            PreparedStatement stmt = con.prepareStatement("""
+                                                            UPDATE COUNTRIES SET 
+                                                            NAME = ?, 
+                                                            NATIONALITY = ?
+                                                            WHERE ID = ?
+                                                            """);
+            stmt.setString(1, country.getName());
+            stmt.setString(2, country.getNationality());
+            stmt.setInt(3, country.getId());
+            return stmt.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(CountryDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    @Override
+    public boolean removeCountry(int id) {
+        try (Connection con = connect()) {
+            Statement stmt = con.createStatement();
+            return stmt.execute("""
+                                DELETE FROM COUNTRIES 
+                                WHERE ID = 
+                                """ + id);
+        } catch (SQLException ex) {
+            Logger.getLogger(CountryDaoImpl.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
     }
 }
--- src/main/java/com/mycompany/dao/inter/CountryDaoInter.java
+++ src/main/java/com/mycompany/dao/inter/CountryDaoInter.java
@@ -4,5 +4,9 @@ import com.mycompany.entity.Country;
 import java.util.List;
 
 public interface CountryDaoInter {
+    boolean addCountry(Country country);
+    Country getCountryById(int id);
     List<Country> getAllCountries();
+    boolean updateCountry(Country country);
+    boolean removeCountry(int id);
 }
