10458c21b2f0ab7d3ddfe8940a1582ba06c0d1de
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-10-14 19:46:34 +0400

    Add instance of SkillDaoImpl

--- src/main/java/com/mycompany/main/Context.java
+++ src/main/java/com/mycompany/main/Context.java
@@ -2,10 +2,12 @@ package com.mycompany.main;
 
 import com.mycompany.dao.impl.CountryDaoImpl;
 import com.mycompany.dao.impl.EmploymentHistoryDaoImpl;
+import com.mycompany.dao.impl.SkillDaoImpl;
 import com.mycompany.dao.impl.UserDaoImpl;
 import com.mycompany.dao.impl.UserSkillDaoImpl;
 import com.mycompany.dao.inter.CountryDaoInter;
 import com.mycompany.dao.inter.EmploymentHistoryDaoInter;
+import com.mycompany.dao.inter.SkillDaoInter;
 import com.mycompany.dao.inter.UserDaoInter;
 import com.mycompany.dao.inter.UserSkillDaoInter;
 
@@ -24,5 +26,9 @@ public class Context {
     
     public static CountryDaoInter instanceOfCountryDao() {
         return new CountryDaoImpl();
+    }
+    
+    public static SkillDaoInter instanceOfSkillDao() {
+        return new SkillDaoImpl();
     }
 }
