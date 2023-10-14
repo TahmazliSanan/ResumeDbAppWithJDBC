d505c2d9509242f0666f72a1a61063f1aff724cf
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-10-14 19:45:44 +0400

    Add other methods

--- src/main/java/com/mycompany/dao/inter/SkillDaoInter.java
+++ src/main/java/com/mycompany/dao/inter/SkillDaoInter.java
@@ -4,5 +4,9 @@ import com.mycompany.entity.Skill;
 import java.util.List;
 
 public interface SkillDaoInter {
+    boolean addSkill(Skill skill);
+    Skill getSkillById(int id);
     List<Skill> getAllSkills();
+    boolean updateSkill(Skill skill);
+    boolean removeSkill(int id);
 }
