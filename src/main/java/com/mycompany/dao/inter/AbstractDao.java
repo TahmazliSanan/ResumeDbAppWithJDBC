7e4c10d86112def2fb2668a48149afc1c5b4ba90
TahmazliSanan <sanan.tahmazli2329@gmail.com>
2023-10-16 14:04:23 +0400

    Modify class

--- src/main/java/com/mycompany/dao/inter/AbstractDao.java
+++ src/main/java/com/mycompany/dao/inter/AbstractDao.java
@@ -5,7 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public abstract class AbstractDao {
-    public Connection connect() throws SQLException {
+    public Connection connect() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
         String url = "jdbc:mysql://localhost:3306/resume";
         String username = "root";
         String password = "23042002";
